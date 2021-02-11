8cc571a4d9abb41d1c1c8ab3f40ef27dec5bf826
matmoom <69425509+matmoom@users.noreply.github.com>
2021-02-11 10:48:15 +0200

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Ashraf | أشرف الطيب`,
+        short_name: `Ashraf`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
